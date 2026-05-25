8bb8f767548e14da5e55215ee19e8637eef5518f
Iposhka54 <yaroslavkusb@gmail.com>
2026-05-25 16:12:09 +0300

    feat: implement standard BFS finder algorithm

---
+++ internal/game/path/path_finder.go
@@ -0,0 +1,96 @@
+package path
+
+import (
+	_map "simulation/internal/game/map"
+	"simulation/internal/game/map/coordinate"
+)
+
+var directions = []coordinate.Coordinate{
+	{X: 0, Y: -1},
+	{X: 0, Y: 1},
+	{X: -1, Y: 0},
+	{X: 1, Y: 0},
+}
+
+type HasAdjacentFood func(c coordinate.Coordinate) bool
+
+type PathFinder struct {
+}
+
+func (pf *PathFinder) Find(worldMap _map.Map,
+	startPosition coordinate.Coordinate,
+	foodChecker HasAdjacentFood) []coordinate.Coordinate {
+	if foodChecker(startPosition) {
+		return []coordinate.Coordinate{startPosition}
+	}
+
+	queue := []coordinate.Coordinate{startPosition}
+	visited := make(map[coordinate.Coordinate]bool)
+	parents := make(map[coordinate.Coordinate]coordinate.Coordinate)
+
+	visited[startPosition] = true
+
+	for len(queue) > 0 {
+		currentPosition := queue[0]
+		queue = queue[1:]
+
+		neighbors := findReachableNeighbors(worldMap, currentPosition)
+
+		for _, neighbor := range neighbors {
+			if visited[neighbor] {
+				continue
+			}
+
+			parents[neighbor] = currentPosition
+
+			if foodChecker(neighbor) {
+				return reconstructPath(neighbor, startPosition, parents)
+			}
+
+			queue = append(queue, neighbor)
+		}
+	}
+
+	return []coordinate.Coordinate{}
+}
+
+func findReachableNeighbors(worldMap _map.Map,
+	startPosition coordinate.Coordinate) []coordinate.Coordinate {
+	neighbors := getNeighbors(startPosition)
+
+	filtered := neighbors[:0]
+
+	for _, neighbor := range neighbors {
+		if worldMap.IsValid(neighbor) && worldMap.IsEmpty(neighbor) {
+			filtered = append(filtered, neighbor)
+		}
+	}
+
+	return filtered
+}
+
+func getNeighbors(startPosition coordinate.Coordinate) []coordinate.Coordinate {
+	var neighbors []coordinate.Coordinate
+
+	for _, dir := range directions {
+		neighbors = append(neighbors, coordinate.Coordinate{
+			X: startPosition.X + dir.X,
+			Y: startPosition.Y + dir.Y,
+		})
+	}
+
+	return neighbors
+}
+
+func reconstructPath(goalCord, startCord coordinate.Coordinate,
+	parents map[coordinate.Coordinate]coordinate.Coordinate) []coordinate.Coordinate {
+	path := []coordinate.Coordinate{goalCord}
+
+	currentCord := goalCord
+	for currentCord != startCord {
+		currentCord = parents[currentCord]
+		path = append([]coordinate.Coordinate{currentCord}, path...)
+	}
+
+	return path
+}
