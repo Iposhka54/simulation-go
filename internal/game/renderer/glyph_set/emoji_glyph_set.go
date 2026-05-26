110494e1ff23477cfb3055c40f1ef7c23b76c323
Iposhka54 <yaroslavkusb@gmail.com>
2026-05-26 18:16:14 +0300

    refactor: removed reflection and replaced it with type casting

--- internal/game/renderer/glyph_set/emoji_glyph_set.go
+++ internal/game/renderer/glyph_set/emoji_glyph_set.go
@@ -1,33 +1,31 @@
 package glyph_set
 
 import (
-	"reflect"
 	"simulation/internal/entity"
 	"simulation/internal/entity/herbivore"
 	"simulation/internal/entity/predator"
 	"simulation/internal/entity/static"
 )
 
-type EmojiGlyphSet struct {
-	glyphs map[reflect.Type]string
-}
+type EmojiGlyphSet struct{}
 
 func NewEmojiGlyphSet() *EmojiGlyphSet {
-	return &EmojiGlyphSet{
-		glyphs: map[reflect.Type]string{
-			reflect.PointerTo(reflect.TypeOf(static.Grass{})):     "🌿",
-			reflect.PointerTo(reflect.TypeOf(static.Tree{})):      "🌳",
-			reflect.PointerTo(reflect.TypeOf(static.Rock{})):      "🪨",
-			reflect.PointerTo(reflect.TypeOf(herbivore.Rabbit{})): "🐇",
-			reflect.PointerTo(reflect.TypeOf(predator.Wolf{})):    "🐺",
-		},
-	}
+	return &EmojiGlyphSet{}
 }
 
 func (e *EmojiGlyphSet) GetGlyph(entity entity.Entity) string {
-	glyph, ok := e.glyphs[reflect.TypeOf(entity)]
-	if !ok {
+	switch entity.(type) {
+	case *static.Grass:
+		return "🌿"
+	case *static.Tree:
+		return "🌳"
+	case *static.Rock:
+		return "🪨"
+	case *herbivore.Rabbit:
+		return "🐇"
+	case *predator.Wolf:
+		return "🐺"
+	default:
 		return "?"
 	}
-	return glyph
 }
